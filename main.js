fea477f208892cb096fd92e26ca7d7e00a4dc31c
Piotr Piwoński <piwonskipiotrp@gmail.com>
2019-04-17 14:55:34 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,12 @@
+const header = document.querySelector("header");
+const h1 = document.querySelector("h1");
+let image = true;
+let title = true;
+
+const changeImage = () => {
+  image = !image;
+  title = !title;
+  header.style.backgroundImage = image ? "url(balony.jpg)" : "url(beee.jpg)";
+  h1.textContent = title ? "Balon" : "Pszczoła";
+};
+header.addEventListener("click", changeImage);
